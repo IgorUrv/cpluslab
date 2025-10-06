5715dab008d1830ec28a664c78801afc18ab8ad1
varona <varona.by@gmail.com>
2025-10-06 11:27:14 +0300

    sort correction

--- tests/sort-test.cpp
+++ tests/sort-test.cpp
@@ -6,29 +6,60 @@
 
 void  sort(int arr[] , int lenght) {
     bool needSort = true;
-
+    int transCount = 0;
     while (needSort) {
         needSort = false;
         for (int  j = 0; j < lenght - 1; j++)
         {
+            transCount++;
             if (arr[j] > arr[j+1])
             {
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
                 needSort = true;
+                
             }
             
         }
-        
     }
+    cout << "Trans Count 1: " << transCount << "\n";
+}
+
+void  sort2(int arr[] , int lenght){
+    int transCount = 0;
+    for (int  i = 0; i < lenght - 1; i++)
+    {
+        for (int  j = 0; j < lenght - 1 - i; j++)
+        {
+            transCount++;
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                
+            }
+            
+        }
+    }
+    cout << "Trans Count 2: " << transCount << "\n";
 }
 
 
-int main(){
+int main() {
     int arr[10] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11};
+    int arr2[10] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11};
     sort(arr, 10);
-     for (int  j = 0; j < 10 - 1; j++) {
-         cout << arr[j] + "\n";
+    sort2(arr2, 10);
+
+     for (int  j = 0; j < 10; j++) {
+         cout << arr[j] << "\n";
      }
+
+          for (int  j = 0; j < 10; j++) {
+         cout << arr2[j] << "\n";
+     }
+
+     cout << "end";
 }
