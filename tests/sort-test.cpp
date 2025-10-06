26fac585836da641d851bc7eb25c1eaa39bd08db
varona <varona.by@gmail.com>
2025-10-06 11:30:23 +0300

    sort correction

--- tests/sort-test.cpp
+++ tests/sort-test.cpp
@@ -48,16 +48,16 @@ void  sort2(int arr[] , int lenght){
 
 
 int main() {
-    int arr[10] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11};
-    int arr2[10] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11};
-    sort(arr, 10);
-    sort2(arr2, 10);
+    int arr[15] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11, 4, 5, 10, 1, 55};
+    int arr2[15] = {1, 20, 3, 40, 50, 2, 120, 11, 20, 11, 4, 5, 10, 1, 55};
+    sort(arr, 15);
+    sort2(arr2, 15);
 
-     for (int  j = 0; j < 10; j++) {
+     for (int  j = 0; j < 15; j++) {
          cout << arr[j] << "\n";
      }
 
-          for (int  j = 0; j < 10; j++) {
+    for (int  j = 0; j < 15; j++) {
          cout << arr2[j] << "\n";
      }
 
